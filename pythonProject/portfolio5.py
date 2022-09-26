28a5f2f973b960dc138fffc4b848e06e6c2853b8
sanajanhaneef <114408263+sanajanhaneef@users.noreply.github.com>
2022-09-26 15:07:46 +0100

    Add files via upload

---
+++ pythonProject/portfolio5.py
@@ -0,0 +1,16 @@
+a = 113
+b = 175
+c = 12
+max = 24
+maxA = a // max
+remainderA = a % max
+maxB = b // max
+remainderB = b % max
+maxC = c // max
+remainderC = c % max
+print (maxA)
+print (remainderA)
+print (maxB)
+print(remainderB)
+print (maxC)
+print(remainderC)
